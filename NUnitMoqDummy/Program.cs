bf01527081d4bb1b737dd6d5a8da41a14acd6e31
Frank Blendinger <fb@intoxicatedmind.net>
2016-11-28 13:50:49 +0100

    add basic product/test structure with MSTest+NUnit

---
+++ NUnitMoqDummy/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace NUnitMoqDummy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Product
+    {
+        public int GetNum()
+        {
+            return 42;
+        }
+    }
+
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void GetNum_WhenCalled_Returns42()
+        {
+            var sut = new Product();
+
+            Assert.That(sut.GetNum(), Is.EqualTo(42 + 23));
+        }
+    }
+}
