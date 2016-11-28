0e0b8ac7d76154e4b77babca2dca330ebe3dffe4
Frank Blendinger <fb@intoxicatedmind.net>
2016-11-28 14:00:41 +0100

    add Moq lib and sample test showing Mock.Verify()

--- NUnitMoqDummy/Program.cs
+++ NUnitMoqDummy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
 
@@ -11,11 +12,21 @@ namespace NUnitMoqDummy
         }
     }
 
+    public interface IClient
+    {
+        void BeCalled(string magicInput);
+    }
+
     public class Product
     {
         public int GetNum()
         {
             return 42;
+        }
+
+        public void CallMe(IClient client)
+        {
+            client.BeCalled("magic");
         }
     }
 
@@ -28,6 +39,17 @@ namespace NUnitMoqDummy
             var sut = new Product();
 
             Assert.That(sut.GetNum(), Is.EqualTo(42 + 23));
+        }
+
+        [TestMethod]
+        public void CallMe_WhenGivenClient_CallsClient()
+        {
+            var clientMock = new Mock<IClient>();
+            var sut = new Product();
+
+            sut.CallMe(clientMock.Object);
+
+            clientMock.Verify(client => client.BeCalled("not really magic"));
         }
     }
 }
